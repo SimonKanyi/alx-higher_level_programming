5a41ed1a85678c2f7394ae9dd955ae5cf4ad3d08
SimonKanyi <simonmainaofficial1@gmail.com>
2024-03-13 11:29:39 -0700

    Functions

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdlib.h>
+
+listint_t *insert_node(listint_t **head, int number)
+{
+    listint_t *new_node = malloc(sizeof(listint_t));
+    if (!new_node)
+        return NULL;
+
+    new_node->n = number;
+    new_node->next = NULL;
+
+    if (!*head || number < (*head)->n)
+    {
+        new_node->next = *head;
+        *head = new_node;
+        return new_node;
+    }
+
+    listint_t *current = *head;
+    while (current->next && current->next->n < number)
+    {
+        current = current->next;
+    }
+
+    new_node->next = current->next;
+    current->next = new_node;
+
+    return new_node;
+}
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+def uppercase(s):
+    for char in s:
+        if ord('a') <= ord(char) <= ord('z'):
+            char = chr(ord(char) - 32)
+        print(char, end="")
+    print()
